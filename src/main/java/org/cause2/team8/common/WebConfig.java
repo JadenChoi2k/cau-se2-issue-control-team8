37513953a024be9908f20a9fb58af169e37ebb3d
JadenChoi2k <jkya02@gmail.com>
2024-06-03 15:51:31 +0900

    fix errors: CORS

--- src/main/java/org/cause2/team8/common/WebConfig.java
+++ src/main/java/org/cause2/team8/common/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
             .allowedOrigins("http://localhost:3000")  // 허용할 출처
-            .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")  // 허용할 HTTP 메서드
             .allowedHeaders("*")  // 허용할 헤더
             .allowCredentials(true);  // 자격 증명 허용
     }
